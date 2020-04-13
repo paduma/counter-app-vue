26780f320bb2877f0361bc6c73d812b5432f6061
paduma <243515893@qq.com>
2020-04-13 12:18:24 +0800

    commit1 - Counters component has Counter component

---
+++ src/components/Counter.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h2 :class="getBadgeClasses()">{{ formatCount() }}</h2>
+    <button class="btn btn-secondary btn-sm" @click="handlerIncrement()">Increment</button>
+    <button class="btn btn-danger btn-sm m-2" @click="deleteCounter(counter.id)">Delete</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      count: this.counter.value
+    }
+  },
+  props: {
+    counter: Object,
+    deleteCounter: Function
+  },
+  methods: {
+    formatCount () {
+      return this.count === 0 ? 'Zero' : this.count
+    },
+    getBadgeClasses () {
+      let classes = 'badge m-2 badge-'
+      classes += this.count === 0 ? 'warning' : 'primary'
+
+      return classes
+    },
+    handlerIncrement () {
+      return this.count++
+    }
+  }
+}
+</script>
---
+++ src/components/Counters.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <Counter v-for="counter in counters" :key="counter.id" :counter="counter" :deleteCounter="deleteCounter" />
+  </div>
+</template>
+
+<script>
+import Counter from './Counter'
+export default {
+  name: 'Counters',
+  components: {
+    Counter
+  },
+  data () {
+    return {
+      counters: [
+        { id: 1, value: 4 },
+        { id: 2, value: 0 },
+        { id: 3, value: 0 },
+        { id: 4, value: 0 }
+      ]
+    }
+  },
+  methods: {
+    deleteCounter (counterId) {
+      this.counters = this.counters.filter(c => c.id !== counterId)
+    }
+  }
+}
+</script>
